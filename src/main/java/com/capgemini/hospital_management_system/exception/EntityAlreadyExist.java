0948ddc65e9e0381cb5a782b096fe15ab0a28223
Rituraj2610 <26rituraj@gmail.com>
2025-06-10 06:41:41 +0530

    [Rituraj] UPDATE . Resolving merge conflicts

--- src/main/java/com/capgemini/hospital_management_system/exception/EntityAlreadyExist.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.hospital_management_system.exception;
-
-public class EntityAlreadyExist extends RuntimeException {
-	public EntityAlreadyExist(String message) {
-		super(message);
-	}
-}
--- src/main/java/com/capgemini/hospital_management_system/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.hospital_management_system.exception;
-
-public class EntityNotFoundException extends RuntimeException{
-	 public EntityNotFoundException(String message) {
-		 super(message);
-	 }
-}
