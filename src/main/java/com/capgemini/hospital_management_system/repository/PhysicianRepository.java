bb3dd290d71b6953aed3554b2c35f1ca3134c0d5
SinghAmoldeep <amoldeepdhillon26@gmail.com>
2025-06-10 19:55:05 +0530

    [Amoldeep] CREATE . Created physicians group by position in department api

--- src/main/java/com/capgemini/hospital_management_system/repository/PhysicianRepository.java
+++ src/main/java/com/capgemini/hospital_management_system/repository/PhysicianRepository.java
@@ -1,7 +1,9 @@
 package com.capgemini.hospital_management_system.repository;
 
+
 import com.capgemini.hospital_management_system.model.Physician;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,14 @@ import java.util.Optional;
 
 @Repository
 public interface PhysicianRepository extends JpaRepository<Physician, Integer> {
+
 	Optional<Physician> findByName(String name);
+
 	List<Physician> findAllByPosition(String position);
+
+	@Query("SELECT p.position, p.name FROM Physician p")
+	List<Object[]> findPositionAndPhysicianNames();
+
+
 
 }
